4024baf261b4ab33a0effabacabcabe40e7f6828
screamatthewind <4452527+screamatthewind@users.noreply.github.com>
2020-11-27 11:56:31 -0500

    Removed notebooks folder

--- notebooks/auto-shutdown/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-apt-get install sysstat bc
-cp ./ashutdown /usr/local/bin/
-cp ./ashutdown.service /lib/systemd/system/
-systemctl --no-reload --now enable /lib/systemd/system/ashutdown.service
